5577219eaf59ff566738bc9cbc8fc81e18fe90c1
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2018-04-09 21:17:10 +0200

    Fix eslint
    Eslint was strange in vscode, this has been fixed.

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,41 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2017,
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": [
-            "error",
-            { allow: ["warn", "error", "log"] }
-        ]
-    }
+	"extends": "react-app",
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"no-console": [
+			"error",
+			{ allow: ["warn", "error", "log"] }
+		]
+	}
 };
